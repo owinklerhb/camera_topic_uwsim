406b7d14c3727aef25cb585a56902362072e36d3
Olaf Winkler <winkler@uni-bremen.de>
2016-08-03 16:07:23 +0200

    Made the code run on UWSim Camera

--- src/CTUWSim.cpp
+++ src/CTUWSim.cpp
@@ -1,10 +1,14 @@
-
 #include <camera_topic_uwsim/CTUWSim.h>
 // System
 #include <iostream>
 #include <cstdlib>
 #include <sstream>
-
+//#include <ros/ros.h>
+//#include <image_transport/image_transport.h>
+//#include <cv_bridge/cv_bridge.h>
+//#include <sensor_msgs/image_encodings.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include "ros/ros.h"
 
 int main(int argc, char** argv) {
@@ -38,53 +42,60 @@ bool CTUWSim::run(std::string strTopicName) {
   return bSuccess;
 }
 
-void CTUWSim::imageCallback(const sensor_msgs::ImagePtr& imgData) { 	//callback bekommt Daten von sensor_msgs im Image Format
-  cv_bridge::CvImagePtr cv_ptr = nullptr;				//absichern, dass cv_ptr nirgendwo hinzeigt
-	sensor_msgs::Image msg;  					//msg wird als sensor_msgs im Format Image deklariert
+void CTUWSim::imageCallback(const sensor_msgs::ImagePtr& imgData) { 		//callback bekommt Daten von sensor_msgs im Image Format
+  cv_bridge::CvImagePtr cv_ptr = nullptr;					//absichern, dass cv_ptr nirgendwo hinzeigt
+  //sensor_msgs::Image msg;  	
+  //msg wird als sensor_msgs im Format Image deklariert
   if(sensor_msgs::image_encodings::isColor(imgData->encoding)) {		//wenn das eingehende Bild BGR8 Format ist, zeigt der cv_ptr darauf
-   cv_ptr = cv_bridge::toCvCopy(imgData, sensor_msgs::image_encodings::BGR8);
+    cv_ptr = cv_bridge::toCvCopy(imgData, sensor_msgs::image_encodings::BGR8); 	//RGB8// BGR16 /// RGBA16 // etc.
     //cv_ptr = cv_bridge::toCvShare(imgData, "rgb8")->image;
   } else {
-    std::cerr << "No color image" << std::endl;				//Fehlermeldung, dass das Bild kein Farbformat hat
+    std::cerr << "No color image" << std::endl;					//Fehlermeldung, dass das Bild kein Farbformat hat
   }
-  
+
   if(cv_ptr) {
-    //cv::Mat imgMat = cv_ptr->image;
+    cv::Mat imgMat = cv_ptr->image; //ROS to OpenCv
 
- 	//cv::imshow("Image", cv_ptr->image);
- 	//if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
-      	//cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));    
+       // TODO: Add CV-related code to process image here.
 
-	
+
+
+    //OPENCV TO ROS
+    //cv::imshow("Image", cv_ptr->image);
+    //	if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
+    //  	cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));    
+    
     //std::cout << cv_ptr->image << std::endl;
+	//std::cout << imgMat.data << std::endl;
     //ROS_INFO_STREAM( cv_ptr->image );
 
     
-    // TODO: Add CV-related code to process image here.
+ 
 
-	
-	
-	ros::NodeHandle n;
-	ros::Publisher imgPub_pub = n.advertise<sensor_msgs::Image>("Image_Pub", 1000);
-	    imgPub_pub.publish(imgData);
+    
+    
+    ros::NodeHandle n;
+    ros::Publisher imgPub_pub = n.advertise<sensor_msgs::Image>("Image_Pub", 1000);
+    imgPub_pub.publish(cv_ptr->toImageMsg());//imgData);
     //std::cout << imgData << std::endl;
-	ros::Rate loop_rate(10);
-
-		int count = 0;
-	  while (ros::ok())
-	  {
-
-	    ros::spinOnce();
-
-	    loop_rate.sleep();
-	    ++count;
-	  }
+    ros::Rate loop_rate(10);
+    
+    int count = 0;
+    while (ros::ok())
+      {
 	
-
+	ros::spinOnce();
+	
+	loop_rate.sleep();
+	++count;
+      }
+    
+    
     // Stop CV-related code here
-
-
+    
+    
   } else {
     std::cerr << "Got no image." << std::endl;
   }
 }
+
