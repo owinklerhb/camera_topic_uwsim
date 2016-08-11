3c956bfe263a13975c11bc311d8ffd8ff417725c
Olaf Winkler <winkler@uni-bremen.de>
2016-08-11 13:26:33 +0200

    Changed a bit of the code and made OpenCV Threshold working

--- src/CTUWSim.cpp
+++ src/CTUWSim.cpp
@@ -11,15 +11,20 @@
 #include <opencv2/highgui/highgui.hpp>
 #include "ros/ros.h"
 
+
 int main(int argc, char** argv) {
   int nReturnvalue = EXIT_FAILURE;
+  
+  //std::cout << "Using OpenCV version: " << CV_VERSION << std::endl;
+  
   
   ros::init(argc, argv, "image_publisher_node");
   ros::NodeHandle nh("~");
   
   CTUWSim::Ptr ctuwsimMain = CTUWSim::create();
   
-  if(ctuwsimMain->run("/uwsim/camera1")) {
+ if(ctuwsimMain->run("/uwsim/camera1")) { 
+ //if(ctuwsimMain->run("/uwsim/rangecamera")) {
     nReturnvalue = EXIT_SUCCESS;
   }
   
@@ -44,7 +49,10 @@ bool CTUWSim::run(std::string strTopicName) {
 
 void CTUWSim::imageCallback(const sensor_msgs::ImagePtr& imgData) { 		//callback bekommt Daten von sensor_msgs im Image Format
   cv_bridge::CvImagePtr cv_ptr = nullptr;					//absichern, dass cv_ptr nirgendwo hinzeigt
-  //sensor_msgs::Image msg;  	
+  //sensor_msgs::Image msg;
+
+
+
   //msg wird als sensor_msgs im Format Image deklariert
   if(sensor_msgs::image_encodings::isColor(imgData->encoding)) {		//wenn das eingehende Bild BGR8 Format ist, zeigt der cv_ptr darauf
     cv_ptr = cv_bridge::toCvCopy(imgData, sensor_msgs::image_encodings::BGR8); 	//RGB8// BGR16 /// RGBA16 // etc.
@@ -58,25 +66,25 @@ void CTUWSim::imageCallback(const sensor_msgs::ImagePtr& imgData) { 		//callback
 
        // TODO: Add CV-related code to process image here.
 
+	 /* Gray scale image */
+            cv::Mat filtered_image;
+            cv::cvtColor(imgMat,filtered_image,CV_BGR2GRAY);
 
-
+	// threshold image 
+	    int global_min_threshold=50; //ggf. löschen
+	    cv::Mat threshold_image;
+	    cv::threshold(filtered_image,imgMat,global_min_threshold,215,cv::THRESH_BINARY_INV);
+	    //cv::namedWindow("Threshold Image");
+	    //cv::imshow("Threshold Image",threshold_image);
+	    
+	    cv::cvtColor(imgMat, cv_ptr->image, CV_GRAY2BGR);
+	
+   
     //OPENCV TO ROS
-    //cv::imshow("Image", cv_ptr->image);
-    //	if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
-    //  	cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));    
-    
-    //std::cout << cv_ptr->image << std::endl;
-	//std::cout << imgMat.data << std::endl;
-    //ROS_INFO_STREAM( cv_ptr->image );
-
-    
- 
-
-    
-    
     ros::NodeHandle n;
     ros::Publisher imgPub_pub = n.advertise<sensor_msgs::Image>("Image_Pub", 1000);
-    imgPub_pub.publish(cv_ptr->toImageMsg());//imgData);
+    //  sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image).toImageMsg() //von Fereshta
+    imgPub_pub.publish(*(cv_ptr->toImageMsg()));//imgData); cv_prt->toImageMsg()
     //std::cout << imgData << std::endl;
     ros::Rate loop_rate(10);
     
